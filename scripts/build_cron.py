5c06d08a082a3a8102a764b53efcd1603de25aef
pikesley <sam.pikesley@gmail.com>
2022-06-20 20:11:55 +0100

    Make `build_cron` testable

--- scripts/build_cron.py
+++ scripts/build_cron.py
@@ -5,6 +5,10 @@ def make_cron_lines(photos_per_minute):
     """Make the cron lines."""
     delay = 0
     lines = []
+
+    if not isinstance(photos_per_minute, int) or photos_per_minute <= 0:
+        return lines
+
     while round(delay) < 60:
         lines.append(
             f"* * * * * pi python {Path.cwd()}/scripts/take_still.py {int(delay)}\n"
@@ -14,11 +18,21 @@ def make_cron_lines(photos_per_minute):
     return lines
 
 
+def write_cron_lines(lines, path):
+    """Write-out the cron lines."""
+    Path(path).write_text("".join(lines), encoding="UTF-8")
+
+
+def retrieve_ppm(path):
+    """Retrieve the photos-per-minute value."""
+    try:
+        return int(Path(path).read_text(encoding="UTF-8"))
+    except (ValueError, FileNotFoundError):
+        return 0
+
+
 if __name__ == "__main__":
-    cron_lines = make_cron_lines(
-        int(Path("conf/photos-per-minute").read_text(encoding="UTF-8"))
-    )
-    Path("/etc", "cron.d", Path.cwd().stem).write_text(
-        "".join(cron_lines),
-        encoding="UTF-8",
+    write_cron_lines(
+        make_cron_lines(retrieve_ppm("conf/photos-per-minute")),
+        f"/etc/cron.d/{Path.cwd().stem}",
     )
--- tests/test_build_cron.py
+++ tests/test_build_cron.py
@@ -1,4 +1,6 @@
-from scripts.build_cron import make_cron_lines
+from pathlib import Path
+
+from scripts.build_cron import make_cron_lines, retrieve_ppm, write_cron_lines
 
 
 def test_build_cron():
@@ -23,3 +25,39 @@ def test_build_cron():
         "* * * * * pi python /opt/timelapser/scripts/take_still.py 42\n",
         "* * * * * pi python /opt/timelapser/scripts/take_still.py 51\n",
     ]
+
+
+def test_invalid_build_cron():
+    """Test it gracefully handles bogus requests."""
+    assert not make_cron_lines(0)
+    assert not make_cron_lines(-1)
+    assert not make_cron_lines("ten")
+
+
+def test_write_cron_lines():
+    """Test it writes out the cron lines."""
+    write_cron_lines(make_cron_lines(3), "/tmp/cronlines")
+
+    output = Path("/tmp/cronlines").read_text(encoding="UTF-8").split("\n")
+    assert output[:3] == [
+        "* * * * * pi python /opt/timelapser/scripts/take_still.py 0",
+        "* * * * * pi python /opt/timelapser/scripts/take_still.py 20",
+        "* * * * * pi python /opt/timelapser/scripts/take_still.py 40",
+    ]
+
+
+def test_retrieve_ppm():
+    """Test it retrieves the photos-per-minute value."""
+    Path("/tmp/ppm").write_text("15", encoding="UTF-8")
+    assert retrieve_ppm("/tmp/ppm") == 15
+
+
+def test_bad_ppm():
+    """Test it gracefully handles duff ppm strings."""
+    Path("/tmp/ppm").write_text("dave", encoding="UTF-8")
+    assert retrieve_ppm("/tmp/ppm") == 0
+
+
+def test_no_ppm():
+    """Test it gracefully handles a missing ppm file."""
+    assert retrieve_ppm("/no/such/path") == 0
